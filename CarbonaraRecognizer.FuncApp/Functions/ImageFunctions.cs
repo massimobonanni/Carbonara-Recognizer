4c2174c7f350b9a0e202b240cb20a2eef0e0794c
Massimo Bonanni <massimo.bonanni@tiscali.it>
2022-08-12 10:49:22 +0200

    Adding integration with event grid topic custom

--- CarbonaraRecognizer.FuncApp/Functions/ImageFunctions.cs
+++ CarbonaraRecognizer.FuncApp/Functions/ImageFunctions.cs
@@ -3,10 +3,12 @@
 using System;
 using System.IO;
 using System.Threading.Tasks;
+using Azure.Messaging.EventGrid;
 using CarbonaraRecognizer.Core.Entities;
 using CarbonaraRecognizer.Core.Interfaces;
 using Microsoft.Azure.Storage.Blob;
 using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.EventGrid;
 using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
@@ -31,6 +33,7 @@ namespace CarbonaraRecognizer.FuncApp.Functions
             string name,
             [Blob("%DestinationContainer%/{name}", FileAccess.ReadWrite, Connection = "DestinationStorageConnectionString")] ICloudBlob validImageClient,
             [Blob("%TrashbinContainer%/{name}", FileAccess.ReadWrite, Connection = "DestinationStorageConnectionString")] ICloudBlob trashImageClient,
+            [EventGrid(TopicEndpointUri = "TopicEndpoint", TopicKeySetting = "TopicKey")] IAsyncCollector<EventGridEvent> eventCollector,
             ILogger log)
         {
             log.LogInformation($"C# Blob trigger function Processed blob\n Name:{name}");
@@ -70,6 +73,15 @@ namespace CarbonaraRecognizer.FuncApp.Functions
                 {
                     await clientToUse.UploadFromStreamAsync(stream);
                 }
+
+                var @event = new EventGridEvent(
+                   subject: inputImageClient.Uri.ToString() ,
+                   eventType: result.IsRecognized ? "imageRecognized" : "imageNotRecognized",
+                   dataVersion: "1.0",
+                   data: (inputImageClient,result));
+
+                await eventCollector.AddAsync(@event);
+
 
                 log.LogTrace($"Image Deleting: clientToUse={inputImageClient.Name}");
                 await inputImageClient.DeleteAsync();
